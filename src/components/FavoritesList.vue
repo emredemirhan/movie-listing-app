6d65b2d25b9cab24c92e18809242218b63b09390
Emre Demirhan <emre.demirhan@tui.com>
2024-11-27 13:41:52 +0300

    feat: add FavoritesList component to the codebase

---
+++ src/components/FavoritesList.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="movie-list-container">
+      <h1>Favorites</h1>
+      <div v-if="favoritesStore.favorites.length" class="movie-grid">
+        <MovieItem
+          v-for="movie in favoritesStore.favorites"
+          :key="movie.imdbID"
+          :movie="movie"
+        />
+      </div>
+      <p v-else>No favorites yet.</p>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import { useFavoritesStore } from '@/stores/favorites';
+  import MovieItem from './MovieItem.vue';
+  
+  const favoritesStore = useFavoritesStore();
+  </script>
+
+  
