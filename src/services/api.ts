003e96fa8c7c0325afa8afcb6b7420480356fc98
Emre Demirhan <emre.demirhan@tui.com>
2024-11-27 14:50:56 +0300

    feat: move api base url to the .env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { MovieApiResponse } from '@/types/movie';
 
-const API_BASE_URL = 'https://jsonmock.hackerrank.com/api/movies';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getMovies = async (page = 1): Promise<MovieApiResponse> => {
     const response = await axios.get<MovieApiResponse>(`${API_BASE_URL}/?page=${page}`);
